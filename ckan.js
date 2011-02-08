3087a921f6a28ca302ba866a18d4c99fa84081f2
rgrp <http://rufuspollock.org>
2011-02-08 00:37:19 +0000

    [js][s]: refactor big search function into subfunctions and a new CKAN.UI module.

--- ckan.js
+++ ckan.js
@@ -1,3 +1,5 @@
+var showdown = new Showdown.converter();
+
 var CKAN = CKAN || {};
 
 CKAN.Remote = function($) {
@@ -10,39 +12,49 @@ CKAN.Remote = function($) {
   return my;
 }(jQuery);
 
-var CKAN = function($, my) {
-  var showdown = new Showdown.converter();
+CKAN.UI = function($) {
+  var my = {};
 
-  my.apiUrlSearch = CKAN.Remote.apiSearch + '/package?q='
+  my.search = function(q) {
+    var apiUrlSearch = CKAN.Remote.apiSearch + '/package?q='
+    var url = apiUrlSearch + q + '&limit=10&all_fields=1';
+    $.ajax({
+      url: url,
+      success: my.renderSearchResults,
+      dataType: 'jsonp'
+    });
+  };
+
+  my.renderSearchResults = function(data) {
+    var $results = $('#results');
+    $results.find('.count').html(data.count);
+    $results.show();
+
+    $(data.results).each(function(idx, item) {
+      item.ckan_url = CKAN.Remote.url + '/package/' + item.name;
+
+      item.displaytitle = item.title ? item.title : item.name;
+
+      item.snippet = $(showdown.makeHtml(item.notes ? item.notes : '')).text();
+      if (item.snippet.split(' ') > 190) {
+        item.snippet = item.snippet.split(' ').slice(0, 190).join(' ') + ' ...';
+      }
+
+      // for templating (to be ckan compatible)
+      item.package = item;
+    });
+    var out = $('#tmpl-package-summary').tmpl(data.results);
+    $results.find('.packages').html(out);
+  };
+
+  return my;
+}(jQuery);
+
+var CKAN = function($, my) {
   my.initialize = function() {
     $('#search-form').submit(function() {
       var q = $('input.search').val();
-      var url = my.apiUrlSearch + q + '&limit=10&all_fields=1';
-      $.ajax({
-        url: url,
-        success: function(data) {
-          var $results = $('#results');
-          $results.find('.count').html(data.count);
-          $results.show();
-
-          $(data.results).each(function(idx, item) {
-            item.ckan_url = CKAN.Remote.url + '/package/' + item.name;
-
-            item.displaytitle = item.title ? item.title : item.name;
-
-            item.snippet = $(showdown.makeHtml(item.notes ? item.notes : '')).text();
-            if (item.snippet.split(' ') > 190) {
-              item.snippet = item.snippet.split(' ').slice(0, 190).join(' ') + ' ...';
-            }
-
-            // for templating (to be ckan compatible)
-            item.package = item;
-          });
-          var out = $('#tmpl-package-summary').tmpl(data.results);
-          $results.find('.packages').html(out);
-        },
-        dataType: 'jsonp'
-      });
+      CKAN.UI.search(q);
       return false;
     });
   };
