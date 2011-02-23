e3b75fe242a61c5c191f9a768ce190f3e09c8948
rgrp <http://rufuspollock.org>
2011-02-23 19:47:38 +0000

    [wui,config][m]: set configuration form (for e.g. api key) in web user interface (and set defaults from local config file).

--- ckan.js
+++ ckan.js
@@ -2,12 +2,16 @@ var showdown = new Showdown.converter();
 
 var CKAN = CKAN || {};
 
-CKAN.Remote = function($) {
+CKAN.Catalog = function($) {
   var my = {};  
-  my.url = CKAN.Config.url;
-  my.api = my.url + '/api';
-  my.apiSearch = my.api + '/search';
-  my.apiRest = my.api + '/rest';
+
+  my.configure = function(url, apikey) {
+    my.url = url;
+    my.api = my.url + '/api';
+    my.apiSearch = my.api + '/search';
+    my.apiRest = my.api + '/rest';
+    my.apikey = apikey;
+  }
 
   my.notify = function(msg, type) {
     $.event.trigger('notification', [msg, type]);
@@ -47,24 +51,45 @@ CKAN.Remote = function($) {
 CKAN.UI = function($) {
   var my = {};
   
-  my.setApiKey = function(q) {
-
-  };
-
-  my.showNotification = function(e, msg, type) {
-    alert(msg);
-  }
-
   my.initialize = function() {
     my.$results = $('#results');
     my.$dialog = $('#dialog'); 
+    my.$ckanUrl = $('#config-form input[name=ckan-url]');
+    my.$apikey = $('#config-form input[name=ckan-api-key]');
+
     $(document).bind('notification', my.showNotification);
     $('#search-form').submit(function() {
       var q = $('input.search').val();
       my.search(q);
       return false;
     });
+
+    // initialize from file config
+    my.$ckanUrl.val(CKAN.Config.url);
+    my.$apikey.val(CKAN.Config.apikey ? CKAN.Config.apikey : '');
+    my.configureCatalog();
+
+    $('.config-show a').click(function(e) {
+      e.preventDefault();
+      $('#config-form').toggle('fast');
+    });
+
+    $('#config-form').submit(function(e) {
+      e.preventDefault();
+      my.$ckanUrl.val(my.$ckanUrl.val());
+      my.$apikey.val(my.$apikey.val());
+      my.configureCatalog();
+      $('#config-form').hide('slow');
+    });
   };
+
+  my.configureCatalog = function() {
+    CKAN.Catalog.configure(my.$ckanUrl.val(), my.$apikey.val());
+  };
+
+  my.showNotification = function(e, msg, type) {
+    alert(msg);
+  }
 
   my.showSpinner = function() {
     my.$dialog.empty();
@@ -77,7 +102,7 @@ CKAN.UI = function($) {
   };
 
   my.search = function(q) {
-    var apiUrlSearch = CKAN.Remote.apiSearch + '/package?q='
+    var apiUrlSearch = CKAN.Catalog.apiSearch + '/package?q='
     var url = apiUrlSearch + q + '&limit=10&all_fields=1';
     my.showSpinner();
     my.$results.hide();
@@ -93,7 +118,7 @@ CKAN.UI = function($) {
     $results.find('.count').html(data.count);
 
     $(data.results).each(function(idx, item) {
-      item.ckan_url = CKAN.Remote.url + '/package/' + item.name;
+      item.ckan_url = CKAN.Catalog.url + '/package/' + item.name;
 
       item.displaytitle = item.title ? item.title : item.name;
 
@@ -113,9 +138,9 @@ CKAN.UI = function($) {
   };
   
   my.makeEditable = function() {
-    $('.editable').editable(CKAN.Remote.saveFromEditable);
+    $('.editable').editable(CKAN.Catalog.saveFromEditable);
     $('.editable-area').editable(
-      CKAN.Remote.saveFromEditable, {
+      CKAN.Catalog.saveFromEditable, {
         type      : 'textarea',
         cancel    : 'Cancel',
         submit    : 'OK',
