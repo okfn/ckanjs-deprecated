a4d48b043db2e3da9f20135cd7eb9c3f8fa2ee88
rgrp <http://rufuspollock.org>
2011-05-12 02:19:58 +0100

    [package/view][m]: implement inline addition of resources (though no save to server yet).
    * tests: really starting to get the hang of this.

--- test/test_view.js
+++ test/test_view.js
@@ -9,9 +9,31 @@ test("PackageSummaryView", function () {
   });
   view.render();
   var tmpl = $(view.el);
-  console.log(tmpl.html());
   var title = tmpl.find('.title a').text();
   equals(title, 'A Novel By Tolstoy');
+});
+
+test("PackageFullView", function () {
+  var pkg = new CKAN.Model.Package(packages[0]);
+  var $view = $('<div />').appendTo($('.fixture'));
+  var view = new CKAN.View.PackageFullView({
+    el: $view,
+    model: pkg
+  });
+  view.render();
+  var tmpl = $(view.el);
+  var title = tmpl.find('.title a').text();
+  equals(title, 'A Wonderful Story');
+
+  $('.action-add-resource').click();
+  var out = tmpl.find('form.resource');
+  equals(out.length, 1, 'Did not find resource form');
+  tmpl.find('form input[name=Resource--url]').val('http://xyz.org');
+  tmpl.find('form.resource').submit();
+  equals(pkg.get('resources').length, 1);
+
+  var out = $view.find('.resources table tr:last td:first').text();
+  ok(out.indexOf('Download (no description)')!=-1, 'Did not find required string');
 });
 
 test("PackageSearchView", function () {
