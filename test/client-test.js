5c056038333610d1bbe18207a7707253bc6c1802
Aron Carroll <self@aroncarroll.com>
2011-07-08 11:37:03 +0100

    Added .syncDataset() and .createDataset() to the Client

--- test/client-test.js
+++ test/client-test.js
@@ -49,4 +49,49 @@
     same(returned, client, 'Expect the client object to have been returned');
   });
 
+  test(".createDataset()", function () {
+    var client = new CKAN.Client(),
+        spy = this.spy(),
+        dataset;
+
+    this.stub(CKAN.Model, 'Dataset').returns({bind: spy});
+
+    dataset = client.createDataset({title: 'My new dataset'});
+
+    ok(CKAN.Model.Dataset.calledOnce, 'Expect it to return an instance of CKAN.Model.Dataset');
+    ok(spy.calledOnce, 'Expect it to call bind on the returned model');
+    ok(spy.calledWith('sync', client.syncDataset), 'Expect it to bind to the "sync" event with client.syncDataset()');
+  });
+
+  test(".syncDataset()", function () {
+    var client = new CKAN.Client(),
+        method  = 'create',
+        options = {},
+        dataset = new CKAN.Model.Dataset(),
+        returned;
+
+    this.spy(Backbone, 'sync');
+    this.stub(client, 'environment').returns('stubbed');
+
+    returned = client.syncDataset(method, dataset, options);
+
+    equal(returned, client, 'Expect it to return the client instance');
+    ok(Backbone.sync.calledOnce, 'Expect it to call Backbone.sync()');
+    ok(Backbone.sync.calledWith(method, dataset), 'Expect it to call Backbone.sync() with the method and dataset');
+    deepEqual(Backbone.sync.args[0][2], {
+      url: 'stubbed' + '/package',
+      headers: {'X-CKAN-API-KEY': 'stubbed'}
+    }, 'Expect it to call Backbone.sync() with "header" and "url" options');
+    ok(client.environment.calledTwice, 'Expect it to call client.environment twice');
+    ok(client.environment.calledWith('api-key'), 'Expect it to request the "api-key" from the client');
+    ok(client.environment.calledWith('rest-endpoint'), 'Expect it request the "rest-endpoint" from the client');
+
+    dataset.id = 1;
+    client.syncDataset('update', dataset, options);
+    deepEqual(Backbone.sync.args[1][2], {
+      url: 'stubbed/package/1',
+      headers: {'X-CKAN-API-KEY': 'stubbed'}
+    }, 'Expect the url to have the id included when the dataset has an id');
+  });
+
 })(this.jQuery);
