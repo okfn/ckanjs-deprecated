79b499fd33b80f360519bd63ec8a9205e91ed595
Aron Carroll <self@aroncarroll.com>
2011-07-08 10:58:58 +0100

    Added unit tests for CKAN.Model.Base

--- test/model-test.js
+++ test/model-test.js
@@ -2,7 +2,34 @@
   * Model tests
   *****************************************************/
 
-module("Model");
+(function ($, undefined) {
+
+module("Model.Base");
+
+test('new Model.Base()', function () {
+  this.spy(Backbone.Model.prototype, 'constructor');
+
+  var args = {name: 'My Model Name'};
+  var instance = new CKAN.Model.Base(args);
+
+  ok(Backbone.Model.prototype.constructor.calledOnce, 'Expect Base to call Backbone.Model');
+  ok(Backbone.Model.prototype.constructor.calledWith(args), 'Expect Base to pass on arguments to Backbone.Model');
+});
+
+test('.sync()', function () {
+  var instance = new CKAN.Model.Base(),
+      method = "create",
+      options = {};
+
+  this.spy(instance, 'trigger');
+
+  instance.sync(method, instance, options)
+
+  ok(instance.trigger.calledOnce, 'Expect .sync() to trigger a "sync" event');
+  ok(instance.trigger.calledWith('sync', method, instance, options), 'Expect .sync() to pass on arguments to listeners');
+});
+
+module("Model.Dataset");
 
 test("Create dataset", function () {
   var indata = {
@@ -17,3 +44,5 @@ test("Create dataset", function () {
   equals(out.notesHtml, '<h2>Xyz</h2>');
   equals(out.displaytitle, indata.title);
 });
+
+})(this.jQuery);
