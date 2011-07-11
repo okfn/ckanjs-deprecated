779caf1f0cdc815d59717b60f3bc51b7db104768
Aron Carroll <self@aroncarroll.com>
2011-07-11 13:33:31 +0100

    Added support for Dataset model having a collection of Resources. Ticket #5

--- test/model-test.js
+++ test/model-test.js
@@ -31,7 +31,7 @@ test('.sync()', function () {
 
 module("Model.Dataset");
 
-test("Create dataset", function () {
+test("new Model.Dataset()", function () {
   var indata = {
     title: 'My New Dataset',
     notes: '## Xyz',
@@ -43,6 +43,53 @@ test("Create dataset", function () {
   var out = pkg.toTemplateJSON();
   equals(out.notesHtml, '<h2>Xyz</h2>');
   equals(out.displaytitle, indata.title);
+});
+
+test(".set({resources: []})", function () {
+  var dataset = new CKAN.Model.Dataset(),
+      resources = dataset.get('resources'),
+      newResources = new Backbone.Collection();
+
+  this.spy(dataset, '_updateResources');
+
+  equals(resources.constructor, Backbone.Collection, 'The resources attribute should be a Backbone collection');
+  equals(resources.model, CKAN.Model.Resource, 'Should create Resource instances');
+  equals(resources.length, 0, 'The resources collection should be empty');
+
+  dataset.set({});
+  ok(!dataset._updateResources.calledOnce, 'Expected collection._updateResources() NOT to have been called');
+
+  dataset.set({resources: newResources});
+  ok(!dataset._updateResources.calledOnce, 'Expected collection._updateResources() NOT to have been called');
+  equal(newResources, dataset.get({resources}), 'Expected the new collection to be set to the "resources" key')
+
+  dataset.set({resources: [{url: "http://pathtonewresource.com/download"}]});
+  ok(dataset._updateResources.calledOnce, 'Expected collection._updateResources() to have been called');
+
+  dataset.set({resources: []});
+  ok(dataset._updateResources.calledTwice, 'Expected collection._updateResources() to have been called');
+});
+
+test("._updateResources()", function () {
+  var dataset = new CKAN.Model.Dataset(),
+      resources = dataset.get('resources'),
+      existingModels = [new CKAN.Model.Resource({id: 1}), new CKAN.Model.Resource({id: 2})],
+      newModels = [{id: 3}, {id: 4}, {id: 2, title: 'New title'}];
+
+  resources.add(existingModels);
+
+  this.spy(resources, 'add');
+  this.spy(resources, 'remove');
+  this.spy(existingModels[1], 'set');
+
+  dataset._updateResources(newModels);
+  ok(resources.add.calledTwice, 'Expected resources.add() to have been called');
+  ok(resources.add.calledWith(newModels[0]), 'Expected resources.add() to have been called with model');
+  ok(resources.add.calledWith(newModels[1]), 'Expected resources.add() to have been called with model');
+  ok(existingModels[1].set.calledWith(newModels[2]), 'Expected model.set() to have been called with new data');
+
+  ok(resources.remove.calledOnce, 'Expected resources.remove() to have been called');
+  ok(resources.remove.calledWith([existingModels[0]]), 'Expected resources.remove() to have been called with array of models');
 });
 
 module("Model.SearchCollection");
